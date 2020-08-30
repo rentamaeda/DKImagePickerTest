4e7d193a93691fd8cc6e9be524b72f5b59311cb3
rentamaeda <japan_lunch_fate@yahoo.co.jp>
2020-08-30 17:08:07 +0900

    collectionViewへimageの渡し方がわからない

---
+++ DKimagePickerTest/CollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCell.swift
+//  DKimagePickerTest
+//
+//  Created by user on 2020/08/30.
+//  Copyright © 2020 user. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+}
--- DKimagePickerTest/ViewController.swift
+++ DKimagePickerTest/ViewController.swift
@@ -7,14 +7,57 @@
 //
 
 import UIKit
+import Photos
+import DKImagePickerController  // 忘れないように
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource  {
 
-class ViewController: UIViewController {
-
+    @IBOutlet weak var CollerctionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        CollerctionView.delegate = self
+              CollerctionView.dataSource = self
     }
 
+    @IBAction func Button(_ sender: Any) {
+        let pickerController = DKImagePickerController()
+        // 選択可能な枚数を20にする
+        pickerController.maxSelectableCount = 20
+        pickerController.didSelectAssets = { [unowned self] (assets: [DKAsset]) in
+            
+            // 選択された画像はassetsに入れて返却されるのでfetchして取り出す
+            for asset in assets {
+                asset.fetchFullScreenImage(completeBlock: { (image, info) in
+                    // ここで取り出せる
+                    //self.imageView.image = image
 
+                })
+            }
+            
+            
+        }
+        
+        self.present(pickerController, animated: true) {}
+    }
+   func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+                  return 13 //追加
+              }
+            func numberOfSections(in collectionView: UICollectionView) -> Int {
+                  return 1
+              }
+       
+            func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+                  //①
+                  let cell = self.CollerctionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! CollectionViewCell
+                  //②
+                  cell.imageView.image = image
+                  //③
+               
+             
+                //⑤
+                 return cell
+                           
+              }
+        
+    
 }
 
